1b7760ef00abbe82db0f753246d61b9a7c6b9d00
Poompong-b <bsaachirs@gmail.com>
2021-04-28 14:17:19 +0700

    Edit Community

--- src/components/FullPost/index.js
+++ src/components/FullPost/index.js
@@ -5,67 +5,72 @@ import parse from 'html-react-parser';
 import './index.css';
 
 const FullPost = ({ post, upVote, downVote }) => {
-
 	return (
 		<div className="fullPane">
-			
-				<div>
-					<div id={post.id} className="full-postPane">
-						<div className="full-infoPane">
-							<div className="full-votePane">
-								<button
-									onClick={() => upVote(post)}
-									className="full-voteUpBT"
-								>
-									⬆
-						</button>
+			<div>
+				<div id={post.id} className="full-postPane">
+					<div className="full-infoPane">
+						<div className="full-votePane">
+							<button
+								onClick={() => upVote(post)}
+								className="full-voteUpBT"
+							>
+								⬆
+							</button>
 
-								{post.voteUp}
+							{post.voteUp}
 
-								<button
-									onClick={() => downVote(post)}
-									className="full-voteDownBT"
-								>
-									⬇
-						</button>
-							</div>
-							<p>Comments</p>
+							<button
+								onClick={() => downVote(post)}
+								className="full-voteDownBT"
+							>
+								⬇
+							</button>
 						</div>
-
-						<div>
-							<div className="full-post">
-								<div>
-									<img
-										src={profilePic}
-										alt="full-profile picture"
-										className="full-profilePic"
-									/>
-								</div>
-
-								<div className="full-postInfo">
-									<div className="full-postBy">
-										<p className="full-displayName">Display Name</p>
-										<p className="full-username">@{post.userid}</p>
-										<p className="full-timestamp">
-											{' '}
-									- {post.timeStamp}
-										</p>
-									</div>
-									<div className="postContent">
-										{post.content?<p>{parse(post.content)}</p>:<div></div>}
-										
-										{/* แสดง Post */}
-									</div>
-								</div>
-							</div>
-						</div>
+						<p>Comments</p>
 					</div>
-					{/* Comments */}
-					<div className="commentPane">
-						<Comment />
+
+					<div>
+						<div className="full-post">
+							<div>
+								<img
+									src={profilePic}
+									alt="full-profile picture"
+									className="full-profilePic"
+								/>
+							</div>
+
+							<div className="full-postInfo">
+								<div className="full-postBy">
+									<p className="full-displayName">
+										Display Name
+									</p>
+									<p className="full-username">
+										@{post.userid}
+									</p>
+									<p className="full-timestamp">
+										{' '}
+										- {post.timeStamp}
+									</p>
+								</div>
+								<div className="postContent">
+									{post.content ? (
+										<p>{parse(post.content)}</p>
+									) : (
+										<div></div>
+									)}
+
+									{/* แสดง Post */}
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
-
+				{/* Comments */}
+				<div className="commentPane">
+					<Comment />
+				</div>
+			</div>
 		</div>
 	);
 };
--- src/components/FullSubCom/index.jsx
+++ src/components/FullSubCom/index.jsx
@@ -2,11 +2,48 @@ import React, { useState, useEffect } from 'react';
 import './index.css';
 
 const FullSubCom = ({ subCom }) => {
-	// const [subCom,setSubCom] = useState({})
+	//States
+	const [edit, setEdit] = useState(false);
 
 	useEffect(() => {
-		console.log(subCom)
-	}, [])
+		console.log(subCom);
+	}, []);
+
+	//Functions
+	const handleOnClick = () => {
+		setEdit(!edit);
+	};
+
+	const handleChange = (e) => {
+		const name = e.target.name;
+		const value = e.target.value;
+		// setSubCom({ ...subcom, [name]: value });
+	};
+
+	const clearInput = () => {
+		// setSubCom({ name: '', description: '' });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		// if (subcom.name && subcom.description) {
+		// 	firebase
+		// 		.firestore()
+		// 		.collection('sub_community')
+		// 		.add({
+		// 			...subcom,
+		// 			ownerUID: user.uid,
+		// 			photoURL:
+		// 				'https://cdn.jeab.com/wp-content/uploads/2020/03/wallpaper-for-jeab06.jpg',
+		// 			totalFollow: 0,
+		// 		})
+		// 		.then(() => {
+		// 			e.target.value = '';
+		// 			clearInput();
+		// 			window.location.reload();
+		// 		});
+		// }
+	};
 
 	return (
 		<div className="fullComPane">
@@ -15,9 +52,44 @@ const FullSubCom = ({ subCom }) => {
 					<img src={subCom.photoURL} alt="" className="subComImg" />
 				</div>
 				<h2>{subCom.name}</h2>
-				<h2>{subCom.description}</h2>
-				<button>Edit</button>
-				<button>Follow</button>
+				<p>{subCom.description}</p>
+				<button className="subcom-btn">Follow</button>
+				<button className="edit-btn" onClick={handleOnClick}>
+					{edit ? 'X' : 'Edit'}
+				</button>
+				{edit && (
+					<>
+						<div className="fullsubcomForm">
+							<form action="">
+								<label htmlFor="">Community Picture</label>
+								<h1>Upload Picture Here</h1>
+								<div className="inputForm">
+									<label htmlFor="">Community Name</label>
+									<input
+										type="text"
+										name="name"
+										className="nameInput"
+										placeholder="What's Your Community Name?"
+										onChange={(e) => handleChange(e)}
+									/>
+									<label htmlFor="">Description</label>
+									<textarea
+										id=""
+										cols="30"
+										rows="10"
+										name="description"
+										className="desInput"
+										placeholder="Write something about your community..."
+										onChange={(e) => handleChange(e)}
+									></textarea>
+								</div>
+								<button onClick={handleSubmit} className="btn">
+									Save
+								</button>
+							</form>
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	);
