91cb638f28896609a08552ddab7b2dd0a90ef997
j4m3ee <j4m3ee01@gmail.com>
2021-04-28 11:43:53 +0700

    pull

--- src/firebase/models/user.js
+++ src/firebase/models/user.js
@@ -1,11 +1,12 @@
 class User{
-    constructor(id,totalVote,bio,displayName,photoURL,email){
+    constructor(id,totalVote,bio,displayName,photoURL,email,role){
         this.id = id
         this.totalVote = totalVote
         this.bio = bio
         this.displayName = displayName
         this.photoURL = photoURL
         this.email = email
+        this.role = role
     }
 }
 
