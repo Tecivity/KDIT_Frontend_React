b076bf3fcbbcb7ee23d2fbd1b3742ac4cae4c327
Poompong-b <bsaachirs@gmail.com>
2021-04-28 20:20:56 +0700

    Edit Profile Option

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 
 const Profile = () => {
+	//States
+	const [edit, setEdit] = useState(false);
+
+	//Functions
+	const handleOnClick = () => {
+		setEdit(!edit);
+	};
+
 	return (
 		<div className="profilePane">
 			<h1>Profile Pic HERE</h1>
 			<h2>Name Here</h2>
 			<h3>Bio ต้องมีมั้ยนิ้</h3>
-			<button>Edit</button>
+			<button onClick={handleOnClick}>Edit</button>
+			{edit && (
+				<div className="editProfilePane">
+					<form action="" className="editProfileForm">
+						<label htmlFor="">Profile Picture</label>
+						<h1>Upload Pic Here</h1>
+						<label htmlFor="">Display Name</label>
+						<input type="text" name="displayNames" />
+						<label htmlFor="">Username</label>
+						<input type="text" name="username" />
+						<button className="btn">Save Changes</button>
+					</form>
+				</div>
+			)}
 			<h3>User's POsts HERE</h3>
 		</div>
 	);
