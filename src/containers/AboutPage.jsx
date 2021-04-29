a53af1ac15a20608a3d3086ac4d947368c9016d7
Poompong-b <bsaachirs@gmail.com>
2021-04-29 13:29:56 +0700

    FIX CSS

--- src/containers/AboutPage.jsx
+++ src/containers/AboutPage.jsx
@@ -5,8 +5,10 @@ const AboutPage = () => {
 	return (
 		<>
 			<Navbar />
-			<div className="centerDiv">
-				<h1 class="head">Connect </h1><h1 class="orangeHead">NISIT</h1><h1 class="Head"> together</h1>
+			<div className="about">
+				<h1 class="head">Connect </h1>
+				<h1 class="orangeHead">NISIT</h1>
+				<h1 class="Head"> together</h1>
 				<h3>
 					Keddit is a home to many communities in KMITL, endless
 					conversation and authenic human connection.
