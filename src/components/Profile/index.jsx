ea9dfe467ead657044881808ad4a4773aac8563f
Poompong-b <bsaachirs@gmail.com>
2021-05-02 16:00:36 +0700

    Edit Profile CSS

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -2,6 +2,7 @@
 import React, { useState, useContext, useEffect } from 'react';
 import Popup from 'reactjs-popup';
 import { MdCancel } from 'react-icons/md';
+import ReactCrop from 'react-image-crop';
 //Components
 import { SessionApi } from '../../hook/SessionApi';
 import Post from '../PostCard/Post';
@@ -13,6 +14,7 @@ import FileUpload from '../../firebase/FileUpload';
 import './index.css';
 import { getSuggestedQuery } from '@testing-library/dom';
 import { PostService, UserService } from '../../services';
+import 'react-image-crop/dist/ReactCrop.css';
 
 const Profile = ({ id }) => {
 	//States
@@ -23,6 +25,7 @@ const Profile = ({ id }) => {
 	const [displayName, setDisplayName] = useState('');
 	const [bio, setBio] = useState('');
 	const [profile, setProfile] = useState({});
+	const [mouseDown, setMouseDown] = useState(false);
 
 	//Contexts
 	const { user, defaultImage, defaultBanner } = useContext(SessionApi);
@@ -30,7 +33,6 @@ const Profile = ({ id }) => {
 	//Functions
 
 	const updateProfile = (e) => {
-		e.preventDefault();
 		const newProfile = {
 			displayName,
 			bannerURL,
@@ -39,6 +41,7 @@ const Profile = ({ id }) => {
 		};
 		console.log(profile);
 		console.log(newProfile);
+		console.log(photoURL);
 		UserService.updateUser(profile.id, newProfile).then((result) => {
 			console.log('Updated data');
 			setProfile({ ...profile, displayName, bannerURL, photoURL, bio });
@@ -140,64 +143,94 @@ const Profile = ({ id }) => {
 									</div>
 
 									<div className="content">
-										<div className="editProfilePane">
-											<div className="editProfileForm">
-												<label htmlFor="">
+										<div className="editProfileForm">
+											<div
+												className="editProfileForm"
+												style={{
+													borderBottom:
+														'2px solid lightgrey',
+												}}
+											>
+												<h1 htmlFor="">
 													Profile Picture
-												</label>
+												</h1>
 
 												<FileUpload
 													url={photoURL}
 													setUrl={setPhotoURL}
 												/>
 
-												<label htmlFor="">
+												<img
+													src={photoURL}
+													alt=""
+													className="full-editProfilePic"
+													draggable="false"
+												/>
+											</div>
+
+											<div
+												className="editProfileForm"
+												style={{
+													borderBottom:
+														'2px solid lightgrey',
+												}}
+											>
+												<h1 htmlFor="">
 													Banner Picture
-												</label>
+												</h1>
 
 												<FileUpload
 													url={bannerURL}
 													setUrl={setBannerURL}
 												/>
 
-												<label htmlFor="">
-													Display Name
-												</label>
-												<input
-													type="text"
-													name="displayNames"
-													value={displayName}
-													onChange={(e) =>
-														setDisplayName(
-															e.target.value,
-														)
-													}
+												<img
+													src={bannerURL}
+													alt=""
+													className="full-editBannerPic"
+													draggable="false"
 												/>
-
-												<label htmlFor="">Bio</label>
-												<input
-													type="text"
-													name="displayNames"
-													value={bio}
-													onChange={(e) =>
-														setBio(e.target.value)
-													}
-												/>
-
-												<button
-													className="btn"
-													onClick={updateProfile}
-												>
-													<a
-														onClick={close}
-														style={{
-															color: 'white',
-														}}
-													>
-														Save Changes
-													</a>
-												</button>
 											</div>
+
+											<label htmlFor="">
+												Display Name
+											</label>
+											<input
+												type="text"
+												name="displayNames"
+												value={displayName}
+												onChange={(e) =>
+													setDisplayName(
+														e.target.value,
+													)
+												}
+												className="editInput"
+											/>
+
+											<label htmlFor="">Bio</label>
+											<input
+												type="text"
+												name="displayNames"
+												value={bio}
+												onChange={(e) =>
+													setBio(e.target.value)
+												}
+												className="editInput"
+											/>
+
+											<button
+												className="btn"
+												onClick={updateProfile}
+											>
+												<a
+													onClick={close}
+													style={{
+														color: 'white',
+													}}
+												>
+													Save Changes
+												</a>
+											</button>
 										</div>
 									</div>
 								</div>
