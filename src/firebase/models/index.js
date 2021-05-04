1389486ba7752cb2efbff90b71a1eba162528f99
Poompong-b <bsaachirs@gmail.com>
2021-05-05 02:56:36 +0700

    student js

--- src/firebase/models/index.js
+++ src/firebase/models/index.js
@@ -1,13 +1,11 @@
-const Student = require('./student')
-const PostModel  = require('./post')
-const CommentModel = require('./comment')
-const SubComModel = require('./subCom')
-const User = require('./user')
+const PostModel = require('./post');
+const CommentModel = require('./comment');
+const SubComModel = require('./subCom');
+const User = require('./user');
 
 module.exports = {
-    Student,
-    PostModel,
-    CommentModel,
-    SubComModel,
-    User
-}
+	PostModel,
+	CommentModel,
+	SubComModel,
+	User,
+};
